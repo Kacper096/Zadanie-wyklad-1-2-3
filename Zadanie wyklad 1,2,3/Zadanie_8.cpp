9cea7549dc3d3a30fcc9a2fb41949db4e0b63892
Kacper096 <kacpermatuszek2@gmail.com>
2017-11-17 19:02:46 +0100

    Zadania wyklad 1,2,3

--- Zadanie wyklad 1,2,3/Zadanie_8.cpp
+++ Zadanie wyklad 1,2,3/Zadanie_8.cpp
@@ -13,14 +13,18 @@ int Zadanie_8()
 	cout << " Tutaj bede dodawal 5 liczb, ale tylko parzyste. Jednak mozesz tez wpisywac nieparzyste. " << endl;
 
 	
-		for (int i = 1; i < 6; i++)
+		for (int i = 1, k = 0; k < 5; i++)
 		{
 			cout << " Podaj " << i << " liczbe ";
 			cin >> liczba;
 			
 			
-				if ((liczba % 2) == 0)
-					suma += liczba;
+			if ((liczba % 2) == 0)
+			{
+				suma += liczba;
+				k++;
+			}
+			
 			
 		}
 	
